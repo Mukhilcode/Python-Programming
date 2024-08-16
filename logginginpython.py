25eee12d880450ce937b1e70efaef4379ef4be8e
Your Name <you@example.com>
2024-08-16 20:35:58 +0530

    file has to be added

--- logginginpython.py
+++ logginginpython.py
@@ -1,6 +1,27 @@
+import logging
+import os
+
+#create a logger string
+
+logging_string="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_folder="logs"
+log_file="logs/running_log.log"
+os.makedirs(log_folder,exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_string,
+    handlers=[
+        logging.FileHandler(log_file)
+    ]
+
+)
+logger=logging.getLogger("mylog")
+
+
 def add_number(a,b):
     out=a+b
-    print("succesfully executed")
+    logger.info("succesfully executed")
     return out
 
 
